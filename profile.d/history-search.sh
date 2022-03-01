8c9680586a1e366da388083a79a56027dfb38ce5
matal3a0 <matal3a0@gmail.com>
2022-03-01 14:59:58 +0100

    Cleared whitespace

--- profile.d/history-search.sh
+++ profile.d/history-search.sh
@@ -1,10 +1,9 @@
 #History search with arrowkeys
 if [ -t 1 ]
 then
-	        bind '"\x1b\x5b\x41":history-search-backward'
-		        bind '"\x1b\x5b\x42":history-search-forward'
+    bind '"\x1b\x5b\x41":history-search-backward'
+    bind '"\x1b\x5b\x42":history-search-forward'
 fi
-
 
 #Keep history between sessions
 shopt -s histappend
